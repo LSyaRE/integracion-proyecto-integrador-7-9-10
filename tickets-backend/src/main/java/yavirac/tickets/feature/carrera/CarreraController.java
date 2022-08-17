1987fe9a70f9293e910f3fd1bf721272486fda28
Ariel Ibarra <akz.ibarra@yavirac.edu.ec>
2022-08-16 19:59:30 -0500

    Agregado-Login/Front

--- tickets-backend/src/main/java/yavirac/tickets/feature/carrera/CarreraController.java
+++ tickets-backend/src/main/java/yavirac/tickets/feature/carrera/CarreraController.java
@@ -3,6 +3,7 @@ package yavirac.tickets.feature.carrera;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,12 +27,14 @@ public class CarreraController {
     //CRUD
 
     //Create
+    //@PreAuthorize("hasAuthority('USUARIO_CREAR')")
     @PostMapping("/save")
     public Carrera save(@RequestBody Carrera carrera) {
         return carreraService.save(carrera);
     }
 
     //Read
+    //@PreAuthorize("hasAuthority('USUARIO_LEER')")
     @GetMapping("/{id}")
     public Carrera findById(@PathVariable long id){
         return carreraService.findById(id);
@@ -45,12 +48,14 @@ public class CarreraController {
     }
 
     //Delete
+    //@PreAuthorize("hasAuthority('USUARIO_ELIMINAR')")
     @DeleteMapping("/deleteById/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable long id){
         carreraService.deleteById(id);
     }
 
+    //@PreAuthorize("hasAuthority('USUARIO_LEER')")
     @GetMapping("/findAll")
     public List<Carrera> findAll(){
         return carreraService.findAll();
